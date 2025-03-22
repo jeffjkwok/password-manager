ed336dad21352b3d1eab2088d86615b63bb259ab
Jeff Kwok <jeffjkwok@gmail.com>
2025-03-22 04:41:17 -0700

    feat: login method in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,10 +51,44 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Validate Account
     const user = await User.findOne(email);
-    if(!user) {
-        return res.status(401).json({message: "Invalid credentials"})
+    if (!user) {
+      return res.status(401).json({ message: "Account does not exist" });
     }
+
+    // Validate Password
+    const isPasswordValid = await user.isValidPassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Credentials are invalid" });
+    }
+
+    const { accessToken, refreshToken } = generateToken(user._id);
+
+    const expiresAt = newDate();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+
+    user.refreshToken.push({ token: refreshToken, expiresAt });
+
+    user.refreshTokens = user.refreshToken.filter(
+      (token) => token.expiresAt > new Date()
+    );
+
+    user.lastActive = new Date();
+    await user.save();
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        emai: user.email,
+        twoFactorEnabled: user.twoFactorEnabled,
+        salt: user.salt,
+        masterKeyVerifier: user.masterKeyVerifier,
+      },
+      accessToken,
+      refreshToken,
+    });
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).json({ message: "Error logging in" });
